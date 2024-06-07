7b4cea074036106e8994093d4b7da4f4c66a3b39
FengJiaqi <1072335602@qq.com>
2024-06-07 20:27:37 +0800

    gee -- 5

---
+++ Web/5-middleware/gee/router_test.go
@@ -0,0 +1,11 @@
+package gee
+
+func newTestRouter() *router {
+	r := newRouter()
+	r.addRoute("GET", "/", nil)
+	r.addRoute("GET", "/hello/:name", nil)
+	r.addRoute("GET", "/hello/b/c", nil)
+	r.addRoute("GET", "/hi/:name", nil)
+	r.addRoute("GET", "/assets/*filepath", nil)
+	return r
+}
